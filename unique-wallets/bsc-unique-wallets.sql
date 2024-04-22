ae4c25e178c032cef694056469c51bd6282e0722
djaciel <dj_roses@hotmail.com>
2024-04-22 07:26:29 -0600

    update unique wallets queries

--- unique-wallets/bsc-unique-wallets.sql
+++ unique-wallets/bsc-unique-wallets.sql
@@ -1,4 +1,17 @@
 SELECT
-  COUNT(BNB.wallet) as unique_wallets
+  SUM(unique_wallets) as unique_wallets
 FROM
-  query_2430222 AS BNB -- BNB Chain - Holograph Protocol related unique wallets
+(
+  SELECT
+    COUNT(BNB.wallet) as unique_wallets
+  FROM
+    query_2430222 AS BNB -- BNB Chain - Holograph Protocol related unique wallets
+
+  UNION ALL
+
+  SELECT
+    COUNT(BNB.wallet) as unique_wallets
+  FROM
+    query_3410807 AS BNB -- BNB Chain - Holograph Protocol related unique wallets
+
+) AS combined_results
--- unique-wallets/polygon-unique-wallets.sql
+++ unique-wallets/polygon-unique-wallets.sql
@@ -1,4 +1,17 @@
 SELECT
-  COUNT(MATIC.wallet) as unique_wallets
+  SUM(unique_wallets) as unique_wallets
 FROM
-  query_2429958 AS MATIC -- Polygon - Holograph Protocol related unique wallets
+(
+  SELECT
+    COUNT(MATIC.wallet) as unique_wallets
+  FROM
+    query_2429958 AS MATIC -- Polygon - Holograph Protocol related unique wallets
+
+  UNION ALL
+
+  SELECT
+    COUNT(MATIC.wallet) as unique_wallets
+  FROM
+    query_3410800 AS MATIC -- Polygon - Holograph Protocol related unique wallets
+
+) AS combined_results
