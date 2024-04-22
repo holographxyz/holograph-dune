db57329b0d660a5decd6d9c292b49d9e76d2894b
djaciel <dj_roses@hotmail.com>
2024-04-22 07:25:48 -0600

    update protocol transaction queries

--- protocol-transactions/avalanche-protocol-transactions.sql
+++ protocol-transactions/avalanche-protocol-transactions.sql
@@ -1,21 +1,53 @@
-WITH
-  avalanche_holograph_protocol_transactions AS (
-    SELECT
-      tx_hash AS tx,
-      COUNT(index) AS id
-    FROM
-      avalanche_c.logs
-    WHERE
-      block_time >= TIMESTAMP '2023-01-01 00:00:00'
-      AND block_time < TIMESTAMP '2024-01-01 00:00:00'
-      AND (
-        contract_address IN (SELECT contract_address FROM query_2357969) -- Avalanche - deployed holographable contracts
-        OR contract_address IN (SELECT contract_address FROM query_2357917) -- Holograph Protocol contract addresses
-      )
-    GROUP BY
-      tx_hash
-  )
+SELECT SUM(total_transactions) AS total_transactions FROM (
+WITH avalanche_holograph_protocol_transactions AS (
+  SELECT
+    tx_hash AS tx,
+    COUNT(index) AS id
+  FROM avalanche_c.logs
+  WHERE
+    block_time >= TRY_CAST('2023-01-01 00:00:00' AS TIMESTAMP)
+    AND block_time < TRY_CAST('2024-01-01 00:00:00' AS TIMESTAMP)
+    AND (
+      contract_address IN (
+        SELECT 
+          contract_address 
+        FROM query_2357969
+      ) -- Avalanche - deployed holographable contracts
+      OR contract_address IN (
+        SELECT 
+          contract_address 
+        FROM query_2357917
+      ) -- Holograph Protocol contract addresses
+    )
+  GROUP BY
+    tx_hash
+), avalanche_holograph_protocol_transactionsV2 AS (
+  SELECT
+    tx_hash AS tx,
+    COUNT(index) AS id
+  FROM avalanche_c.logs
+  WHERE
+    block_time >= TRY_CAST('2024-01-01 00:00:00' AS TIMESTAMP)
+    AND block_time < TRY_CAST('2026-01-01 00:00:00' AS TIMESTAMP)
+    AND (
+      contract_address IN (
+        SELECT 
+          contract_address 
+        FROM query_3410746
+      ) -- Avalanche - deployed holographable contracts V2
+      OR contract_address IN (
+        SELECT 
+          contract_address 
+        FROM query_3410710
+      ) -- Holograph Protocol contract addresses V2
+    )
+  GROUP BY
+    tx_hash
+)
 SELECT
-  COUNT(avalanche_holograph_protocol_transactions.tx) AS total_transactions
-FROM
-  avalanche_holograph_protocol_transactions
+  COUNT(tx) AS total_transactions
+FROM avalanche_holograph_protocol_transactions
+UNION ALL
+SELECT
+  COUNT(tx) AS total_transactions
+FROM avalanche_holograph_protocol_transactionsV2)
--- protocol-transactions/protocol-transactions-over-time.sql
+++ protocol-transactions/protocol-transactions-over-time.sql
@@ -4,31 +4,7 @@ SELECT
     SELECT
       COALESCE(SUM(tx), 0)
     FROM
-      query_2611087
-    WHERE
-      block_time <= days.day
-  ) AS "Arbitrum",
-  (
-    SELECT
-      COALESCE(SUM(tx), 0)
-    FROM
-      query_2464251
-    WHERE
-      block_time <= days.day
-  ) AS "Avalanche",
-  (
-    SELECT
-      COALESCE(SUM(tx), 0)
-    FROM
-      query_2464261
-    WHERE
-      block_time <= days.day
-  ) AS "BNB Chain",
-  (
-    SELECT
-      COALESCE(SUM(tx), 0)
-    FROM
-      query_2464267
+      query_3643607
     WHERE
       block_time <= days.day
   ) AS "Ethereum",
@@ -36,59 +12,113 @@ SELECT
     SELECT
       COALESCE(SUM(tx), 0)
     FROM
-      query_2517310
+      query_3652976
     WHERE
       block_time <= days.day
-  ) AS "Optimism",
+  ) AS "Polygon",
   (
     SELECT
       COALESCE(SUM(tx), 0)
     FROM
-      query_2464217
+      query_3652964
     WHERE
       block_time <= days.day
-  ) AS "Polygon",
+  ) AS "BNB Chain",
+  (
+    SELECT
+      COALESCE(SUM(tx), 0)
+    FROM
+      query_3652984
+    WHERE
+      block_time <= days.day
+  ) AS "Avalanche",
+  (
+    SELECT
+      COALESCE(SUM(tx), 0)
+    FROM
+      query_3643906
+    WHERE
+      block_time <= days.day
+  ) AS "OP Mainnet",
+  (
+    SELECT
+      COALESCE(SUM(tx), 0)
+    FROM
+      query_3652981
+    WHERE
+      block_time <= days.day
+  ) AS "Arbitrum",
+  (
+    SELECT
+      COALESCE(SUM(tx), 0)
+    FROM
+      query_3652990
+    WHERE
+      block_time <= days.day
+  ) AS "Zora",
+  (
+    SELECT
+      COALESCE(SUM(tx), 0)
+    FROM
+      query_3652988
+    WHERE
+      block_time <= days.day
+  ) AS "Base",
   COALESCE(
     (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2611087
+        query_3643607
       WHERE
         block_time <= days.day
     ) + (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2464251
+        query_3652976
       WHERE
         block_time <= days.day
     ) + (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2464261
+        query_3652964
       WHERE
         block_time <= days.day
     ) + (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2464267
+        query_3652984
       WHERE
         block_time <= days.day
     ) + (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2517310
+        query_3643906
       WHERE
         block_time <= days.day
     ) + (
       SELECT
         COALESCE(SUM(tx), 0)
       FROM
-        query_2464217
+        query_3652981
+      WHERE
+        block_time <= days.day
+    ) + (
+      SELECT
+        COALESCE(SUM(tx), 0)
+      FROM
+        query_3652990
+      WHERE
+        block_time <= days.day
+    ) + (
+      SELECT
+        COALESCE(SUM(tx), 0)
+      FROM
+        query_3652988
       WHERE
         block_time <= days.day
     ),
