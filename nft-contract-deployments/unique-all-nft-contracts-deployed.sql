b2c5284c35e7eae455d2b3c08cd9e54f9dc3f172
facu <facundogregori@hotmail.com>
2024-04-25 17:45:13 -0300

    refact home section querys

---
+++ nft-contract-deployments/unique-all-nft-contracts-deployed.sql
@@ -0,0 +1,27 @@
+Select
+  SUM(
+    CASE
+      WHEN dataType = 'NFTs' THEN dataValue
+      ELSE 0
+    END
+  ) as totalNft,
+  SUM(
+    CASE
+      WHEN dataType = 'Contracts' THEN dataValue
+      ELSE 0
+    END
+  ) as totalContracts
+From
+  (
+    SELECT
+      dataType,
+      dataValue
+    FROM
+      query_3200193
+    union
+    SELECT
+      dataType,
+      dataValue
+    FROM
+      query_3410769
+  )
